852511b8b1621ab9b285a8bf2582828c7c59c1db
KarmanyaT <61420434+KarmanyaT28@users.noreply.github.com>
2021-05-24 16:58:37 +0530

    Add files via upload

---
+++ DataStructures/Basic C++ Programs/Basic2.cpp
@@ -0,0 +1,13 @@
+//program to find the last digit of a number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1;
+	int last;
+	cin>>num1;
+	last=num1%10;
+	cout<<last;
+	return 0;
+}
