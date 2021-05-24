2c75a9781294073312b0bebec886033eea0bf401
KarmanyaT <61420434+KarmanyaT28@users.noreply.github.com>
2021-05-24 16:53:44 +0530

    Add files via upload

---
+++ DataStructures/Basic C++ Programs/Basic1.cpp
@@ -0,0 +1,17 @@
+//program to find the remainder of a number
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	typedef int s;
+	s num1;
+	s num2;
+	cin>>num1;
+	cin>>num2;
+	s result;
+	result = num1%num2;
+	cout<<result<<endl;
+	return 0;
+}
